c6f1d6f38c2f57948dc2eb2dabd7525e7dd80c43
Haxwwwww <xd.wsj@163.com>
2021-03-27 11:14:37 +0800

    解决了view的bug

--- client/src/components/View.vue
+++ client/src/components/View.vue
@@ -4,14 +4,14 @@
     <div id="triangle"></div>
 
     <span :style="{ marginRight: '8px' }">Categories:</span>
-    <template v-for="tag in tags" :key="tag">
+    <div v-for="tag in tags" :key="tag">
       <a-checkable-tag
-        :checked="selectedTags.indexOf(tag) gi> -1"
+        :checked="selectedTags.indexOf(tag) !== -1"
         @change="(checked) => handleChange(tag, checked)"
       >
         {{ tag }}
       </a-checkable-tag>
-    </template>
+    </div>
   </div>
 </template>
 
@@ -58,17 +58,17 @@ export default {
       checked3: false,
       tags: ["Movies", "Books", "Music", "Sports"],
       selectedTags: ["Movies", "Books", "Music", "Sports"],
-      unselectedTags:[]
+      unselectedTags: [],
     };
   },
   methods: {
     handleChange(tag, checked) {
       //console.log("tag:",tag);
       const { selectedTags } = this;
-      const{}
+      const {};
       const nextSelectedTags = checked
-        ? [...selectedTags, tag]//如果用户“选中”，就在selectedTag中添加这个新标签；
-        : selectedTags.filter((t) => t !== tag);//如果用户取消选择，就在selectedTag中把除了这个标签之外的标签筛选出来
+        ? [...selectedTags, tag] //如果用户“选中”，就在selectedTag中添加这个新标签；
+        : selectedTags.filter((t) => t !== tag); //如果用户取消选择，就在selectedTag中把除了这个标签之外的标签筛选出来
       console.log("You are interested in: ", nextSelectedTags);
       this.selectedTags = nextSelectedTags;
     },
