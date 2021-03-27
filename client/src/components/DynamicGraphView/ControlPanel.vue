de984e83c22570ad7c6b90225e091ed3cbc9bc83
Dy <daiyong@zju.edu.cn>
2021-03-28 00:20:20 +0800

    增加了rect

--- client/src/components/DynamicGraphView/ControlPanel.vue
+++ client/src/components/DynamicGraphView/ControlPanel.vue
@@ -1,33 +1,31 @@
 <template>
   <div style="height: 100%">
-    <div class="panel-header">
-      Prismatic
-    </div>
+    <div class="panel-header">Prismatic</div>
     <div class="panel-header-end"></div>
     <div>
       <a-row>
-        <a-col :span="9" style="line-height: 40px; font-size: 14px;">
+        <a-col :span="9" style="line-height: 40px; font-size: 14px">
           Select stocks and correlation
         </a-col>
         <a-col :span="12">
           <a-slider
-              range
-              :tooltipVisible="false"
-              v-model:value="correlationRange"
-              :min="-1"
-              :max="1"
-              :step="0.01"
-              :marks="correlationMarks"
-              @change="onCorrelationSliderChange"
+            range
+            :tooltipVisible="false"
+            v-model:value="correlationRange"
+            :min="-1"
+            :max="1"
+            :step="0.01"
+            :marks="correlationMarks"
+            @change="onCorrelationSliderChange"
           />
         </a-col>
         <a-col :span="3" align="middle" style="padding: 2px">
           <a-button
-              shape="circle"
-              type="primary"
-              :loading="correlationButtonLoading"
-              :disabled="stockButtonDisabled"
-              @click="onCorrelationButtonClick"
+            shape="circle"
+            type="primary"
+            :loading="correlationButtonLoading"
+            :disabled="stockButtonDisabled"
+            @click="onCorrelationButtonClick"
           >
             <template #icon><FilterOutlined /></template>
           </a-button>
@@ -35,21 +33,21 @@
       </a-row>
       <a-row type="flex" class="panel">
         <a-select
-            style="
+          style="
             width: 100%;
             border: 1px solid #aeaeae;
             box-shadow: 0 10px 10px -6px rgba(21, 85, 194, 0.13);
             border-radius: 5px;
           "
-            v-model:value="stockSelected"
-            mode="multiple"
-            allowClear
-            placeholder="Search by stocks"
-            option-label-prop="label"
+          v-model:value="stockSelected"
+          mode="multiple"
+          allowClear
+          placeholder="Search by stocks"
+          option-label-prop="label"
         >
           <a-select-option
-              disabled
-              style="
+            disabled
+            style="
               color: steelblue;
               font-size: 14px;
               line-height: 50px;
@@ -70,13 +68,13 @@
             </p>
           </a-select-option>
           <a-select-option
-              v-for="stock in stockList"
-              :key="stock.ts_code"
-              :label="stock.ts_code"
-              style="font-size: 12px"
+            v-for="stock in stockList"
+            :key="stock.ts_code"
+            :label="stock.ts_code"
+            style="font-size: 12px"
           >
             <p
-                style="
+              style="
                 text-align: center;
                 width: 20%;
                 display: inline-block;
@@ -87,7 +85,7 @@
               {{ stock.ts_code }}
             </p>
             <p
-                style="
+              style="
                 text-align: center;
                 width: 25%;
                 display: inline-block;
@@ -98,7 +96,7 @@
               {{ stock.name }}
             </p>
             <p
-                style="
+              style="
                 text-align: center;
                 width: 25%;
                 display: inline-block;
@@ -109,7 +107,7 @@
               {{ stock.level1 }}
             </p>
             <p
-                style="
+              style="
                 text-align: center;
                 width: 30%;
                 display: inline-block;
@@ -125,64 +123,65 @@
     </div>
 
     <a-divider
-        style="font-size: 18px; font-weight: bold; padding-top: 10px; margin: 0px"
-    >Correlated clusters</a-divider>
+      style="font-size: 18px; font-weight: bold; padding-top: 10px; margin: 0px"
+      >Correlated clusters</a-divider
+    >
     <dynamic-graph-view
-        class="dynamic-graph"
-        :corr-distribution="corrDistribution"
-        :corr-cluster="corrCluster"
-        @clicked-year="onYearClick"
+      class="dynamic-graph"
+      :corr-distribution="corrDistribution"
+      :corr-cluster="corrCluster"
+      @clicked-year="onYearClick"
     >
     </dynamic-graph-view>
 
     <a-divider
-        style="
+      style="
         font-size: 18px;
         font-weight: bold;
         padding-top: 10px;
         padding-bottom: 2px;
         margin: 0px;
       "
-    >Business tag filters</a-divider
+      >Business tag filters</a-divider
     >
     <a-row justify="space-around" class="tag-cards">
       <a-tabs tabPosition="top" size="small">
         <a-tab-pane key="L1" tab="Industry" force-render>
           <a-tag
-              color="purple"
-              v-for="tag in businessTag.L1"
-              :key="tag"
-              style="margin: 5px"
+            color="purple"
+            v-for="tag in businessTag.L1"
+            :key="tag"
+            style="margin: 5px"
           >
             {{ tag.name }}({{ tag.count }})
           </a-tag>
         </a-tab-pane>
         <a-tab-pane key="L2" tab="Sector">
           <a-tag
-              color="blue"
-              v-for="tag in businessTag.L2"
-              :key="tag"
-              style="margin: 5px"
+            color="blue"
+            v-for="tag in businessTag.L2"
+            :key="tag"
+            style="margin: 5px"
           >
             {{ tag.name }}({{ tag.count }})
           </a-tag>
         </a-tab-pane>
         <a-tab-pane key="L3" tab="Subsector">
           <a-tag
-              color="cyan"
-              v-for="tag in businessTag.L3"
-              :key="tag"
-              style="margin: 5px"
+            color="cyan"
+            v-for="tag in businessTag.L3"
+            :key="tag"
+            style="margin: 5px"
           >
             {{ tag.name }}({{ tag.count }})
           </a-tag>
         </a-tab-pane>
         <a-tab-pane key="Concept" tab="Concept">
           <a-tag
-              color="orange"
-              v-for="tag in businessTag.concept"
-              :key="tag"
-              style="margin: 5px"
+            color="orange"
+            v-for="tag in businessTag.concept"
+            :key="tag"
+            style="margin: 5px"
           >
             {{ tag.name }}({{ tag.count }})
           </a-tag>
@@ -212,17 +211,24 @@ export default {
   emits: ["get-correlation-matrix"],
   data() {
     return {
-      stockList: [{ts_code:'000001.SH', name:'上证指数', level1:'index', level3:'composite'}],
+      stockList: [
+        {
+          ts_code: "000001.SH",
+          name: "上证指数",
+          level1: "index",
+          level3: "composite",
+        },
+      ],
       stockSelectionNumMax: 10,
-      stockSelected: ['000652', '000538'],
+      stockSelected: ["000652", "000538"],
 
       corrDistribution: index_corr_dist,
 
       correlationRange: [0.7, 1],
       correlationMarks: {
-        0.7: {'style': {'top': '-30px', 'font-size': '12px'}, 'label': '0.7'},
-        0: {'style': {'top': '-30px', 'font-size': '12px'}, 'label': '0'},
-        1: {'style': {'top': '-30px', 'font-size': '12px'}, 'label': '1'},
+        0.7: { style: { top: "-30px", "font-size": "12px" }, label: "0.7" },
+        0: { style: { top: "-30px", "font-size": "12px" }, label: "0" },
+        1: { style: { top: "-30px", "font-size": "12px" }, label: "1" },
       },
       correlationButtonLoading: false,
 
@@ -255,32 +261,40 @@ export default {
   },
   methods: {
     onCorrelationSliderChange(val) {
-      this.correlationMarks = { 0: {'style': {'top': '-32px', 'font-size': '12px'}, 'label': '0'} };
-      this.correlationMarks[val[0]] = {'style': {'top': '-32px', 'font-size': '12px'}, 'label': ''+val[0]};
-      this.correlationMarks[val[1]] = {'style': {'top': '-32px', 'font-size': '12px'}, 'label': ''+val[1]};
+      this.correlationMarks = {
+        0: { style: { top: "-32px", "font-size": "12px" }, label: "0" },
+      };
+      this.correlationMarks[val[0]] = {
+        style: { top: "-32px", "font-size": "12px" },
+        label: "" + val[0],
+      };
+      this.correlationMarks[val[1]] = {
+        style: { top: "-32px", "font-size": "12px" },
+        label: "" + val[1],
+      };
     },
     onCorrelationButtonClick() {
       this.correlationButtonLoading = !this.correlationButtonLoading;
       DataService.post(
-          "get_corr_clusters_all_years",
-          this.correlationRange,
-          (data) => {
-            this.correlationButtonLoading = !this.correlationButtonLoading;
-            this.corrCluster = data;
-          }
+        "get_corr_clusters_all_years",
+        this.correlationRange,
+        (data) => {
+          this.correlationButtonLoading = !this.correlationButtonLoading;
+          this.corrCluster = data;
+        }
       );
       DataService.post(
-          "get_business_tag_table",
-          this.correlationRange,
-          (data) => {
-            this.businessTag = data;
-          }
+        "get_business_tag_table",
+        this.correlationRange,
+        (data) => {
+          this.businessTag = data;
+        }
       );
     },
     onYearClick(topNodes, year) {
       // TODO: put a button to select filter
       // eslint-disable-next-line no-constant-condition
-      let nodes = true? topNodes: this.corrCluster[year].nodes;
+      let nodes = true ? topNodes : this.corrCluster[year].nodes;
       this.$emit("get-correlation-matrix", nodes, year);
     },
   },
@@ -323,5 +337,22 @@ export default {
 .tag-cards {
   height: 210px;
   overflow: auto;
+}
+
+/* 滚动槽 */
+::-webkit-scrollbar {
+  width: 6px;
+  height: 6px;
+}
+::-webkit-scrollbar-track {
+  border-radius: 3px;
+  background: rgba(0, 0, 0, 0.06);
+  -webkit-box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.08);
+}
+/* 滚动条滑块 */
+::-webkit-scrollbar-thumb {
+  border-radius: 3px;
+  background: rgba(0, 0, 0, 0.12);
+  -webkit-box-shadow: inset 0 0 10px rgba(0, 0, 0, 0.2);
 }
 </style>
